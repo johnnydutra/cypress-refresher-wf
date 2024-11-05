08e4fc67deee43bfe861f7f087ff60ba51db608c
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-05 09:18:56 -0300

    course 2 content

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    experimentalRunAllSpecs: true,
     viewportHeight: 880,
     viewportWidth: 1280,
-
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
